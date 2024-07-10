10e6db95fee759c58b7820172bf308da85462942
Goiat <tiagosilva2517.ts@gmail.com>
2024-07-10 13:39:04 -0300

    fisrt commit

---
+++ src/test/java/TGS/BOOT_CAMP/BootCampApplicationTests.java
@@ -0,0 +1,13 @@
+package TGS.BOOT_CAMP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
